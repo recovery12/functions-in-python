750c9efd4502b0c00df414d60ea66063fd81d30e
Anuhyapeddi <42506400+Anuhyapeddi@users.noreply.github.com>
2019-10-14 19:07:47 +0530

    to pass parameters by keywords in functions

---
+++ keyword.py
@@ -0,0 +1,10 @@
+# keyword arguments 
+def tpye_something(name = "something", age = 27):
+    print("my name is", name, "and my age is", age)
+    
+# to call function 
+# to pass by specific values
+type_something(name = "Nick", age = 27)
+
+# to pass only one value 
+type_something(age = 27)
